c3ed609bbe341588abb671e7a377a602115747f9
Callum Evans <callumevans@outlook.com>
2017-08-21 17:22:50 +0100

    Fluent API for adding parameters and headers (#59)

--- src/FluidHttp/Extensions/FluidRequestExecutorExtensions.cs
+++ src/FluidHttp/Extensions/FluidRequestExecutorExtensions.cs
@@ -4,6 +4,24 @@ namespace FluidHttp
 {
     public static class FluidRequestExecutorExtensions
     {
+        public static FluidRequest WithHeader(this FluidRequest request, string header, string value)
+        {
+            request.SetHeader(header, value);
+            return request;
+        }
+
+        public static FluidRequest WithBodyParameter(this FluidRequest request, string name, object value)
+        {
+            request.AddBodyParameter(name, value);
+            return request;
+        }
+
+        public static FluidRequest WithQueryParameter(this FluidRequest request, string name, object value)
+        {
+            request.AddQueryParameter(name, value);
+            return request;
+        }
+
         public static Task<FluidResponse> FetchAsync(this FluidRequest request)
         {
             return FetchAsync(request, null);
